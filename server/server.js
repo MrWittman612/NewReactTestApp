456fd146567def89c7c26ebf6a551c165da607aa
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-07-27 00:55:07 -0500

    add file for REST Client ext

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
-const express = require( 'express' )
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get( '/', function ( req, res ) {
-  res.send( 'Hello World' )
-} )
-
+app.get("/", function(req, res) {
+  res.send("Hello World");
+});
 
 const port = process.env.PORT || 9000;
 
-app.listen( port, () => console.log( `Server started on port ${port}` ) );
+app.listen(port, () => console.log(`Server started on port ${port}`));
